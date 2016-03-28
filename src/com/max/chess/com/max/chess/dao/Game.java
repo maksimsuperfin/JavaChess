0854aebab2e28d078f116857aea494c63f8e75bb
maksimsuperfin <maxym.superfin@gmail.com>
2016-03-28 22:14:21 +0300

    Updated logic of displaying deskboard

--- src/com/max/chess/com/max/chess/dao/Game.java
+++ src/com/max/chess/com/max/chess/dao/Game.java
@@ -6,10 +6,11 @@ import java.util.*;
  * Created by User on 27.03.2016.
  */
 public class Game {
+    public static final int deskSize = 8;
     Player player1, player2;
     String color1, color2;
     Map<Player, String> playersColors = new HashMap<Player, String>();
-    int[][] cells = new int[8][8];
+    int[][] cells = new int[deskSize][deskSize];
     public static final int[][] initialCells = {
         {-4, -2, -3, -5, -6, -3, -2, -4},
         {-1, -1, -1, -1, -1, -1, -1, -1},
@@ -20,20 +21,21 @@ public class Game {
         {1, 1, 1, 1, 1, 1, 1, 1},
         {4, 2, 3, 5, 6, 3, 2, 4}
     };
-
-    /*private static final int first_row_idx = 7;
-    private static final int last_row_idx = 0;
-    private static final int first_col_idx = 0;
-    private static final int last_col_idx = 7;*/
+    public static final char[] columnNames = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+    public static final int[] rowNumbers = {1, 2, 3, 4, 5, 6, 7, 8};
 
     public Game(Player player1, Player player2, String color1, String color2, int[][] cells) {
         this.player1 = player1;
         this.player2 = player2;
         this.color1 = color1;
         this.color2 = color2;
+        this.cells = cells;
         playersColors.put(player1, color1);
         playersColors.put(player2, color2);
-        this.cells = cells;
+        Set<Map.Entry<Player, String>> entries = playersColors.entrySet();
+        for (Map.Entry<Player, String> entry: entries) {
+            System.out.println("Player: " + entry.getKey().getName() + " has color " + entry.getValue());
+        }
     }
 
     public Game(Player player1, Player player2, String color1, String color2) {
@@ -45,21 +47,27 @@ public class Game {
     }
 
     private void displayDashBoard() {
-        //System.out.println("Game: displayDashBoard");
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
-                System.out.print(String.format("%-2d", cells[row][col]));
+        displayHeader();
+        for (int row = 0; row < deskSize; row++) {
+            System.out.print(rowNumbers[row] + "|");
+            for (int col = 0; col < deskSize; col++) {
+                System.out.print(String.format("%2d", cells[row][col]));
             }
             System.out.println();
         }
     }
 
+    private void displayHeader() {
+        System.out.print("  ");
+        for (int col = 0; col < deskSize; col++) {
+            System.out.print(String.format("%2s", columnNames[col]));
+        }
+        System.out.println();
+        System.out.println("  ----------------");
+    }
+
     public void currentPosition() {
         System.out.println("Game: currentPosition");
-        Set<Map.Entry<Player, String>> entries = playersColors.entrySet();
-        for (Map.Entry<Player, String> entry: entries) {
-            System.out.println("Player: " + entry.getKey().getName() + " has color " + entry.getValue());
-        }
         displayDashBoard();
     }
 }
